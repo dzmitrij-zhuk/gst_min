d404e667d63053f067db456b8b6cdafed496c79c
Dzmitry Zhuk <d.zhuk@setplex.com>
2024-09-06 16:16:55 +0300

    upd timestamp

--- utils.cpp
+++ utils.cpp
@@ -9,7 +9,7 @@ std::string getTimestamp() {
     const auto nowMs = std::chrono::duration_cast<std::chrono::milliseconds>(
         now.time_since_epoch()) % 1000;
     std::stringstream nowSs;
-    nowSs << std::put_time(std::localtime(&nowAsTimeT), "%F %T")
+    nowSs << std::put_time(std::localtime(&nowAsTimeT), "%F.%T")
             << '.' << std::setfill('0') << std::setw(3) << nowMs.count();
     return nowSs.str();
 }
